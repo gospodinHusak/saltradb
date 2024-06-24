952614ad1455adfd657bdd9f6016c25f3a9b62dd
gospodinHusak <gadart178@gmail.com>
2024-06-24 21:48:00 +0300

    deleted logger

--- funcs.py
+++ funcs.py
@@ -1,41 +1,20 @@
 import psycopg2
-import logging
 import pandas as pd
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(filename='example.log', encoding='utf-8')
-
-logger.setLevel(logging.INFO)
-handler = logging.StreamHandler()
-handler.setLevel(logging.INFO)
-logger.addHandler(handler)
-
-
 
 def get_conn():
-    try:
-        conn = psycopg2.connect(
-            host=os.getenv("HOST"), 
-            port=os.getenv("POSRT"), 
-            dbname=os.getenv("DBNAME"), 
-            user=os.getenv("USER"), 
-            password=os.getenv("PASSWORD")
-        )
+    conn = psycopg2.connect(
+        host=os.getenv("HOST"), 
+        port=os.getenv("POSRT"), 
+        dbname=os.getenv("DBNAME"), 
+        user=os.getenv("USER"), 
+        password=os.getenv("PASSWORD")
+    )
 
-        return conn, conn.cursor()
-    
-    except psycopg2.errors.ConnectionException as e:
-        logger.error(e)
-        raise e
-    
-    except psycopg2.errors.ConnectionFailure as e2:
-        logger.error(e2)
-        raise e2
-    
-    
+    return conn, conn.cursor()
 
 
 def get_data(query): 
